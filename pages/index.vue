fb69bc6124ea45bec218ba373739173c6af2c1f7
국경아 <guk4638@naver.com>
2022-04-12 10:13:30 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+import {storeToRefs} from "pinia"
+import {useCounterStore} from "~/store/counter";
+import {useStore} from "~/store/store";
+
+export default {
+  name: 'IndexPage',
+  setup() {
+    const counter = useCounterStore();
+
+    counter.count++;
+    counter.$patch({ count: counter.count + 1 });
+    counter.increment();
+
+    const store = useStore()
+    const { name, doubleCount } = storeToRefs(store)
+    const { increment } = store
+
+    return {
+      name,
+      doubleCount,
+      increment,
+      store,
+    }
+  }
+}
+</script>
